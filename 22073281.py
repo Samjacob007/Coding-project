765a68d40e3cfb7236709f215e713175d517bbe0
Samjacob007 <samjacob8545.sj@gmail.com>
2024-01-09 20:19:47 +0000

    COMMIT

--- 22073281.py
+++ 22073281.py
@@ -12,14 +12,10 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm, skew, kurtosis
 
 
-data = pd.read_csv("C://Users//sam jacob//OneDrive//Desktop//fundamental of data science//data1-1.csv", names=['Salary'])
-print(data)
-
-# Assuming you have already loaded your data into a DataFrame named 'data'
-# and the salaries are stored in a column named 'Salary'
+data = pd.read_csv("data1.csv", names=['Salary'])
 
 # Plot the histogram
-plt.hist(data['Salary'], bins=30, density=True, alpha=0.7, color='#1f77b4', edgecolor='black')
+plt.hist(data['Salary'], bins=30, density=True, label='PDF',alpha=0.7, color='#1f77b4', edgecolor='black')
 
 # Fit a normal distribution to the data
 mu, std = norm.fit(data['Salary'])
@@ -35,7 +31,7 @@ plt.plot(x, p, 'k', linewidth=2, label='Normal distribution fit')
 mean_salary = np.mean(data['Salary'])
 plt.axvline(mean_salary, color='red', linestyle='dashed', linewidth=2, label=f'Mean Salary ($W$): {mean_salary:.2f}')
 
-# Calculate and print the value X
+# Calculate the value 'X', which is the 67th percentile of the salary data, and plot a vertical line at this value.
 X = np.percentile(data['Salary'], 67)  # 100% - 33% = 67%
 plt.axvline(X, color='purple', linestyle='dashed', linewidth=2, label=f'X: {X:.2f}')
 
